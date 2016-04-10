53dbeb517f7e123bbda108a495a8239cb44e79f0
murabio <fabio.muratorio@gmail.com[A>
2016-04-10 03:56:30 +0200

    Modifica con JSON

--- js/js1.js
+++ js/js1.js
@@ -14,11 +14,13 @@ if (typeof js1 == "undefined") {
             new Array(0, 1, 0, 0, 1),
             new Array(0, 0, 0, 1, 0)
         ),
-        position: 0,
-        oldPosition: -1,
+        positionAbs: 0,
+        positionRel: 0,
+        poi: null,
         timer: null,
 
         init: function() {
+
             js1.person = document.getElementById("person");
             js1.person.style.position = "absolute";
             js1.person.style.left = "290px";
@@ -26,49 +28,51 @@ if (typeof js1 == "undefined") {
         },
 
         start: function() {
-            if (js1.timer != null)
-                clearInterval(js1.timer);
-            var source = document.getElementById("source");
-            var sVal = source.options[source.selectedIndex].value;
+            js1.poi = JSON.parse(js1.GetText("http://localhost:8080/json/json1.json"));
+            var sVal = js1.positionAbs;
             var destination = document.getElementById("destination");
             var dVal = destination.options[destination.selectedIndex].value;
             js1.p = js1.dijkstra(parseInt(sVal), parseInt(dVal));
+            js1.positionRel = 0;
             js1.timer = setInterval(js1.myTimer, 20);
         },
 
         myTimer: function() {
-            var i = js1.position;
+            var i = js1.positionRel;
             var nextPoi = js1.p[i];
             var x = nextPoi[0];
             var y = nextPoi[1];
             var px = js1.personX;
             var py = js1.personY;
-            var op = js1.oldPosition;
 
             if (px < x)
                 js1.personX++;
             else if (px > x)
                 js1.personX--;
+
             if (py < y)
                 js1.personY++;
             else if (py > y)
                 js1.personY--;
-            if (px == x && py == y) {
-                if (i < js1.p.length - 1 && i > op) {
-                    js1.oldPosition = i;
-                    js1.position++;
-                } /*else if (i == js1.p.length - 1 && js1.position > 0) {
-                    js1.oldPosition = i;
-                    js1.position--;
-                } else if (i < js1.p.length - 1 && i > 0 && i < op) {
-                    js1.oldPosition = i;
-                    js1.position--;
-                } */else if (i == 0) {
-                    js1.oldPosition = i;
-                    js1.position++;
-                }
-            }
 
+            if (px == x && py == y) {
+                if (i == js1.p.length - 1) {
+                    clearInterval(js1.timer);
+                }
+                js1.positionRel++;
+            }
+        
+            for (var j = 0; j < js1.poi.p.length; j++) {
+                if (px + 10 == js1.poi.p[j].x && py + 10 == js1.poi.p[j].y)
+                    js1.positionAbs = j;
+            }
+            /**
+             * Commentato perchè per serve solo per debug
+             */
+            // var div = document.getElementById('position');
+            // div.innerHTML = 'Position abs ' + js1.positionAbs;
+            // div.innerHTML += '</br>';
+            // div.innerHTML += 'Position rel ' + (parseInt(js1.positionRel)-1);      
             js1.person.style.left = (js1.personX) + "px";
             js1.person.style.top = (js1.personY) + "px";
         },
@@ -132,9 +136,8 @@ if (typeof js1 == "undefined") {
             for (var i = 0; i < resIndex.length; i++) {
                 result.push(resTot[resIndex[i]]);
             }
-            debugger;
-            js1.person.style.left = result[0][0]+"px";
-            js1.person.style.top = result[0][1]+"px";
+            js1.person.style.left = result[0][0] + "px";
+            js1.person.style.top = result[0][1] + "px";
             js1.personX = result[0][0];
             js1.personY = result[0][1];
             js1.position = 0;
@@ -154,7 +157,15 @@ if (typeof js1 == "undefined") {
                 return 1;
             else
                 return 0;
+        },
+
+        GetText: function(url) {
+            var Httpreq = new XMLHttpRequest(); // a new request
+            Httpreq.open("GET", url, false);
+            Httpreq.send(null);
+            return Httpreq.responseText;
         }
+
     };
     js1.init();
 };
